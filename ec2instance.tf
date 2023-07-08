ee11cde01dcef26ecf5e09a9152ada14a18a5a46
Sami Islam <islam.samipkr@gmail.com>
2023-07-08 14:14:09 -0400

    added

--- ec2instance.tf
+++ ec2instance.tf
@@ -4,3 +4,10 @@ resource "aws_s3_bucket" "bucket1" {
         name="test-bucket-${count.index}"
         }
 }
+resource "aws_instance""myec2m"{
+    ami=data.aws_ami.amz_linux2.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.vpc-ssh,aws_security_group.vpc-web]
+    count = 2
+    tags = "Count-demo-${count.index}"
+}
